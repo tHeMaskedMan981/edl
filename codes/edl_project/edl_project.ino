4c03d758c1196ac35abe5134c06c3a6c2d218e9b
tHeMaskedMan981 <akash.minato@gmail.com>
2019-03-29 15:34:25 +0530

    asdf

---
+++ codes/edl_project/edl_project.ino
@@ -0,0 +1,146 @@
+
+//int analogPin = A0;
+int val = 0;
+int val1 = 0;
+int output = 3;
+int output1 = 4;
+float voltage = 1.0, voltage1 = 1.0,prev_voltage = 1.0, prev_voltage1 = 1.0;
+int n=0, i=0;
+float sum_freq=0, sum_phase=0;
+float avg_freq=0, instant_avg=0;
+float avg_phase=0, instant_avg_phase=0;
+float start_time=512,start_time1=5, stop_time=610; 
+float freq, phase_degree=0, phase_degree2=0; 
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+ 
+pinMode(output, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+ val = analogRead(A0);    // read the input pin
+  val1 = analogRead(A1);    // read the input pin
+voltage = (val/1023.0)*5.0;
+voltage1 = (val1/1023.0)*5.0;
+n+=1;
+//sum+=voltage;
+//avg = sum/n;
+
+int output_voltage = (voltage/5.0)*255;
+int output_voltage1 = (voltage1/5.0)*255;
+if (output_voltage>=127)
+analogWrite(output, 255);
+else if (output_voltage<127)
+analogWrite(output, 0);
+
+if (output_voltage1>=127)
+analogWrite(output1, 255);
+else if (output_voltage1<127)
+analogWrite(output1, 0);
+
+if (output_voltage>127 && prev_voltage<127)
+//if (voltage>127)
+{
+  start_time= millis();
+
+}
+
+if (output_voltage<127&&prev_voltage>127)
+{
+  stop_time= millis();
+// Serial.print("inside loop 2 ");             // debug value
+
+  freq = 500/(stop_time-start_time);
+}
+
+
+if (output_voltage1>127&&prev_voltage1<127)
+{
+  start_time1= millis();
+// Serial.print("inside loop 2 ");             // debug value
+  phase_degree = 0.36*avg_freq*(start_time1 - start_time);
+  phase_degree2 = 360-phase_degree; 
+}
+  
+prev_voltage = output_voltage; 
+prev_voltage1 = output_voltage1; 
+//analogWrite(output, output_voltage);
+//
+//Serial.print(" the current value is : ");             // debug value
+//
+//  Serial.print(voltage );             // debug value
+//  Serial.print(" the output value is : ");             // debug value
+//
+//  Serial.println(output_voltage );             // debug value
+//digitalWrite(output, HIGH);
+//Serial.print(" the avg voltage is : ");             // debug value
+//
+//Serial.print(avg);             // debug value
+if (n==1000){
+  
+//Serial.print(" output_voltage is : ");             // debug value
+////
+//  Serial.print(output_voltage );             // debug value
+////  
+//Serial.print(" the value of n : ");             // debug value
+//Serial.print(n);             // debug value
+//Serial.print(" ");             // debug value
+//Serial.print("start_time : ");             // debug value
+//Serial.print(start_time);             // debug value
+//Serial.print(" ");             // debug value
+//Serial.print("stop_time : ");             // debug value
+//Serial.print(stop_time);             // debug value
+//Serial.print(n);             // debug value
+//Serial.print("  freq : ");             // debug value
+//Serial.print(freq);             // debug value
+//Serial.print("  phase diff : ");             // debug value
+//Serial.print(phase_degree);             // debug value
+//Serial.print("  phase diff2 : ");             // debug value
+//Serial.print(phase_degree2);             // debug value
+//Serial.print("  instant avg : ");             // debug value
+//Serial.print(instant_avg);             // debug value
+//Serial.print("  i : ");             // debug value
+//Serial.print(i);             // debug value
+//Serial.print("  sum freq : ");             // debug value
+//Serial.print(sum_freq);             // debug value
+
+//Serial.print(" the time is : ");             // debug value
+//
+//Serial.println(millis());             // debug value
+n=0;
+    if (i<10)
+  {
+  sum_freq+=freq; 
+  if (phase_degree<phase_degree2)
+  sum_phase+=phase_degree; 
+  else
+  sum_phase+=phase_degree2;
+  i+=1;
+  instant_avg = sum_freq/i; 
+   
+    }
+else 
+{
+  avg_freq = sum_freq/10;
+  avg_phase = sum_phase/10;
+  i=0;
+  sum_freq=0;
+  sum_phase=0;
+  Serial.print("  avg freq : ");             // debug value
+Serial.print(avg_freq);             // debug value
+  Serial.print("  avg phase : ");             // debug value
+Serial.println(avg_phase);             // debug value
+  }
+
+  }
+//  Serial.println("hello");             // debug value
+//delay(3);
+}
+
---
+++ codes/wifi/wifi.ino
@@ -0,0 +1,114 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include<SoftwareSerial.h>
+
+SoftwareSerial s(3,1);
+
+const char* host = "ecell.in";
+
+float voltage=220.0, current=20.0, phase=2.0, power=30.0;
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting");
+  WiFi.begin("despair", "123456789");
+
+  Serial.print("Connecting");
+//  while (WiFi.status() != WL_CONNECTED)
+//  {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  GET();
+}
+
+void loop() {
+
+    
+//    delay(5000);
+//    GET();
+    while (Serial.available()>0)
+    {
+        Serial.println(s.read()) ;  
+      }  
+  }
+
+  // If you only need to post data to the database then you don't need the code below:
+void GET(void)
+{
+//  float voltage=220.0, current=20.0, phase=2.0, power=30.0;
+  HTTPClient http;    //Declare object of class HTTPClient
+ 
+  
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  else {
+    Serial.println("connection success");
+    }
+//String url = "/edl/edl.php";
+String url = "/v1/api/user/userlist";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("Respond:");
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+
+  Serial.print("Sending data : Voltage =  ");
+    Serial.print(voltage);
+    Serial.print(", Current =  ");
+    Serial.print(current);
+    Serial.print(", Phase =  ");
+    Serial.print(phase);
+    Serial.print(", Power =  ");
+    Serial.println(power);
+
+    Serial.print("Sending....");
+    
+  String get_data = "?voltage="+String(voltage)+"&current="+String(current)+"&phase="+String(phase)+"&power="+String(power);
+  String Link = "http://edl.ecell.in/store_data.php"+get_data;  //Requeste webpage
+  
+  Serial.println(Link);
+  http.begin(Link);     //Specify request destination
+ 
+  int httpCode = http.GET();            //Send the request
+  String payload = http.getString();    //Get the response payload
+ 
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+ 
+  http.end();  //Close connection
+  voltage+=2;
+  current+=1.5;
+  power+=20.0;
+  phase+=0.2;
+  
+  delay(5000);  //GET Data at every 5 seconds
+
+}
